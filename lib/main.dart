5b2a709ba5d24c1a109f18f8d219f43107a2ae33
binhphancreator <tienkhach19@gmail.com>
2022-09-28 13:46:24 +0700

    change intl to easy_localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:sunday/routes/routes.dart';
 import 'package:sunday/sunday.dart';
 
-void main() {
-  runApp(Sunday(
-    initRoute: RoutesPage.preloadPage,
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await EasyLocalization.ensureInitialized();
+
+  runApp(EasyLocalization(
+    supportedLocales: const [
+      Locale('en'),
+    ],
+    path: 'assets/langs',
+    fallbackLocale: const Locale('en'),
+    child: Sunday(
+      initRoute: RoutesPage.preloadPage,
+    ),
   ));
 }
